302f47649e060202cccffd7837e51a873cef227b
yuanqi <mutouyuguo@gmail.com>
2025-03-13 15:47:00 +0800

    fix emacs bug

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 VERSION=30.1
+GCC_VERSION=12
 
-apt install -y libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls28-dev pkg-config fontconfig libjansson-dev libgccjit-11-dev fonts-emojione shfmt glslang-tools
+apt install -y libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls28-dev pkg-config fontconfig libjansson-dev libgccjit-$GCC_VERSION-dev fonts-emojione shfmt glslang-tools
 
-cd && wget https://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.xz
-tar -xf emacs-$VERSION.tar.xz && cd emacs-$VERSION || exit
+cd && wget https://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.xz || exit
+tar -xf emacs-$VERSION.tar.xz || exit
+cd emacs-$VERSION || exit
 ./configure --with-native-compilation --with-gnutls --with-json --with-x-toolkit=no
 make -j && make install
 
