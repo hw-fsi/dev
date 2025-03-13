fe82ed47bd1198904364a93ffa6c3849673da69e
yuanqi <mutouyuguo@gmail.com>
2025-03-13 16:16:38 +0800

    texinfo

--- build-emacs.sh
+++ build-emacs.sh
@@ -3,7 +3,7 @@
 VERSION=30.1
 GCC_VERSION=12
 
-apt install -y libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls28-dev pkg-config fontconfig libjansson-dev libgccjit-$GCC_VERSION-dev fonts-emojione shfmt glslang-tools
+apt install -y libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgnutls28-dev pkg-config fontconfig libjansson-dev libgccjit-$GCC_VERSION-dev fonts-emojione shfmt glslang-tools texinfo
 
 cd && wget https://ftp.gnu.org/gnu/emacs/emacs-$VERSION.tar.xz || exit
 tar -xf emacs-$VERSION.tar.xz || exit
