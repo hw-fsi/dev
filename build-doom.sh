0619d030508c94e20041cc90dfad81244b3d203b
mutouyuguo <mutouyuguo@gmail.com>
2025-03-14 17:24:53 +0800

    fix doom interactive

--- build-doom.sh
+++ build-doom.sh
@@ -5,8 +5,9 @@ mkdir ~/.config/doom -p
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.config/emacs
 sed -i '129s|:repo "https://git.savannah.gnu.org/git/emacs/nongnu.git"|:repo "https://github.com/emacsmirror/nongnu_elpa.git"|' ~/.config/emacs/lisp/lib/packages.el
 
-~/.config/emacs/bin/doom install
+yes | ~/.config/emacs/bin/doom install
 cp ~/*.el ~/.config/doom/
+doom sync -j 64
 
 # doom doctor
 apt install -y markdown shellcheck ispell
