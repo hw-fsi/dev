767d92da7916ead59244b091e54d080a3c75e1c9
mutouyuguo <mutouyuguo@gmail.com>
2025-03-14 21:48:44 +0800

    first env then install

--- build-doom.sh
+++ build-doom.sh
@@ -5,7 +5,8 @@ mkdir ~/.config/doom -p
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.config/emacs
 sed -i '129s|:repo "https://git.savannah.gnu.org/git/emacs/nongnu.git"|:repo "https://github.com/emacsmirror/nongnu_elpa.git"|' ~/.config/emacs/lisp/lib/packages.el
 
-yes | ~/.config/emacs/bin/doom install
+~/.config/emacs/bin/doom env
+~/.config/emacs/bin/doom install
 cp ~/*.el ~/.config/doom/
 doom sync -j 64
 
