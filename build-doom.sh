9290813ec09242303e49071186dfc5725945950f
yuanqi <mutouyuguo@gmail.com>
2025-03-14 12:03:14 +0800

    fix doom sync bug

--- build-doom.sh
+++ build-doom.sh
@@ -3,12 +3,13 @@
 # doom emacs
 mkdir ~/.config/doom -p
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.config/emacs
+sed -i '129s|:repo "https://git.savannah.gnu.org/git/emacs/nongnu.git"|:repo "https://github.com/emacsmirror/nongnu_elpa.git"|' ~/.config/emacs/lisp/lib/packages.el
 
 ~/.config/emacs/bin/doom install
 cp ~/*.el ~/.config/doom/
 
 # doom doctor
-apt install -y shellcheck ispell
+apt install -y markdown shellcheck ispell
 
 # clear
 cd && rm ./*.el -rf
